0d2f35eccf1981de232fe688c53b47ecf0957a05
amelcanss <116529661+amelcanss@users.noreply.github.com>
2022-10-28 20:58:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -372,7 +372,7 @@ global.hwaifu = ['https://i.pinimg.com/originals/ed/34/f8/ed34f88af161e6278993e1
 'https://i.pinimg.com/originals/80/4f/1a/804f1a05f9996c96a2d492b4854b7fd5.jpg']
 /*============== STICKER WM ==============*/
 global.stickpack = 'By Fahil'
-global.stickauth = ` Wangsaf Botz X By Fahil`
+global.stickauth = ` instagram : @_pahilz26`
 
 global.multiplier = 38 // The higher, The harder levelup
 
