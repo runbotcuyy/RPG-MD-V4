bbff668fa7c352d2ff441d033d53dbafeb16b7b6
amelcanss <116529661+amelcanss@users.noreply.github.com>
2022-11-01 16:28:52 +0700

    Update quotes-galau.js

--- plugins/quotes-galau.js
+++ plugins/quotes-galau.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-    await conn.sendButton(m.chat, `“${pickRandom(global.galau)}”`, '© by xynoz', 'Galau', [[`${usedPrefix + command}`]], m)
+    await conn.sendButton(m.chat, `“${pickRandom(global.galau)}”`, '© by fahil', 'Galau', [[`${usedPrefix + command}`]], m)
 }
 handler.help = ['galau']
 handler.tags = ['quotes']
